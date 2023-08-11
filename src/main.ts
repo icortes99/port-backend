7dcef1de59ad1444a70e6597326156535c953325
Ivan <cortes.ivan353@gmail.com>
2023-08-11 10:15:09 -0600

    Cors policy updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: 'http://localhost:8000',
+    methods: ['GET', 'PUT', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 204
+  })
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
